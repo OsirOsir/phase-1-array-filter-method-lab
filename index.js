4f83e0bb3b96584cb0a01252f591b8324b2477ae
Philip osir Otieno <philiposir2@gmail.com>
2024-07-02 05:05:23 +0300

    arrayfilters lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(collection, name) {
+  return collection.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(collection, name) {
+  return collection.filter(driver => driver.charAt(0, 2) === name.charAt(0, 2))
+}
+
+function matchName(collection, name) {
+  return collection.filter(driver => driver.name === name)
+}
